0aef353b881471a084a696e6e497d549fef4b340
diptesh6501 <diptesh1993@gmail.com>
2020-02-26 10:46:31 +0530

    Issue #SB-17576 fix: four layouts for mcq

--- projects/quml-library/src/lib/mcq/mcq.component.ts
+++ projects/quml-library/src/lib/mcq/mcq.component.ts
@@ -19,11 +19,12 @@ export class McqComponent implements OnInit , AfterViewInit {
   mcqQuestion: any;
   options: any;
   mcqOptions: any[] = [];
-  isExpanded: boolean = true;
+  layoutValue = 'layout1'; // to be removed
+  isExpanded: Boolean = true;
   @Input() public layout?: string;
   @Output() componentLoaded = new EventEmitter<any>();
   @Output() answerChanged = new EventEmitter<any>();
-  elementRefer:any;
+  elementRefer: any;
   @ViewChild('question') questionTag: any;
   @Output() optionSelected = new EventEmitter<number>();
 
@@ -37,7 +38,7 @@ export class McqComponent implements OnInit , AfterViewInit {
     this.componentLoaded.emit({event: 'mcq component has been loaded'});
     this.renderLatex();
     this.questions = this.questions ? this.questions : questionData;
-    this.layout = this.layout ? this.layout : 'new';
+    this.layout = this.layout ? this.layout : 'layout1';
     if (this.questions['__cdata'] != null) {
       const parsedQuestions = JSON.parse(this.questions.__cdata);
       this.mcqQuestion = this.domSanitizer.sanitize(SecurityContext.HTML,
